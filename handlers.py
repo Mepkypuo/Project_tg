868d9196762cd2793692ffa3e20165b17a44dec2
Максим <Maxim@Maxim-Air.local>
2023-12-14 10:05:41 +0300

    изменение файла .gitognore, в handlers.py добавил lower

--- handlers.py
+++ handlers.py
@@ -11,7 +11,7 @@ router = Router()
 async def start_handler(msg: Message):
     await msg.answer(text.greet.format(name=msg.from_user.full_name), reply_markup=kb.menu)
 
-@router.message(F.text == "Меню")
+@router.message(F.text.lower() == "меню")
 @router.message(F.text == "Выйти в меню")
 @router.message(F.text == "◀️ Выйти в меню")
 async def menu(msg: Message):
